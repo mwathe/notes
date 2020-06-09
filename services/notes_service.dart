030ccd639d76e953e4e2c48727095087b86ee83e
alan mwathe <41288693+mwathe@users.noreply.github.com>
2020-06-09 09:44:54 +0300

    Add files via upload

---
+++ services/notes_service.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+import 'package:notes/models/api_response.dart';
+import 'package:notes/models/note.dart';
+import 'package:notes/models/note_for_listing.dart';
+import 'package:http/http.dart' as http;
+
+class NotesService {
+  static const API = 'http://api.notes.programmingaddict.com';
+  static const headers = {'apiKey': '4c457c1c-77bd-4901-b919-c2917a16ce5c'};
+
+  Future<APIResponse<List<NoteForListing>>> getNotesList() {
+    return http.get(API + '/notes', headers: headers).then((data) {
+      if (data.statusCode == 200) {
+        final jsonData = json.decode(data.body);
+        final notes = <NoteForListing>[];
+        for (var item in jsonData) {
+          notes.add(NoteForListing.fromJson(item));
+        }
+        return APIResponse<List<NoteForListing>>(data: notes);
+      }
+      return APIResponse<List<NoteForListing>>(error: true, errorMessage: 'An error occured');
+    })
+        .catchError((_) => APIResponse<List<NoteForListing>>(error: true, errorMessage: 'An error occured'));
+  }
+
+  Future<APIResponse<Note>> getNote(String noteID) {
+    return http.get(API + '/notes/' + noteID, headers: headers).then((data) {
+      if (data.statusCode == 200) {
+        final jsonData = json.decode(data.body);
+        return APIResponse<Note>(data: Note.fromJson(jsonData));
+      }
+      return APIResponse<Note>(error: true, errorMessage: 'An error occured');
+    })
+        .catchError((_) => APIResponse<Note>(error: true, errorMessage: 'An error occured'));
+  }
+}
